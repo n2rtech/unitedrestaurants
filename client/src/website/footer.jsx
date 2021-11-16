cb816959bd79314cc1d9c4d9090da4fbda7fcdb2
Avinash <kavinash@n2rtechnologies.in>
2021-11-16 19:45:59 +0530

    new changes is admin

--- client/src/website/footer.jsx
+++ client/src/website/footer.jsx
@@ -1,17 +1,67 @@
 import React,{useState} from 'react';
-import { Container, Row, Col, Form, FormGroup, Input, Label, Button,NavItem, NavLink, Nav,TabContent,TabPane } from 'reactstrap'
+import { Container, Row, Col, List, Form, FormGroup, Input, Label, Button,NavItem, NavLink, Nav,TabContent,TabPane } from 'reactstrap'
 import './css/style.css'
 const Footer = (props) => {
 
-  return (
-      <Container fluid={true} className="p-0">
-      <Row className="m-0">
-        <Col xs="12" className="p-0">     
-          FOOTER
-        </Col>
-        </Row>
-      </Container>
-  );
+return (
+<div className="mainfooter">
+	<Container fluid={true}>
+		<Row className="m-0">
+			<Col sm="4" xs="12">     
+			<div className="socialmenu">
+				<a href="#"><i className="fa fa-facebook"></i></a>
+				<a href="#"><i className="fa fa-twitter"></i></a>
+				<a href="#"><i className="fa fa-google-plus"></i></a>
+				<a href="#"><i className="fa fa-linkedin"></i></a>
+				<a href="#"><i className="fa fa-instagram"></i></a>
+			</div>
+			<div className="copyright">
+				<p>© Copyright 2021 Gabal Global Group</p>
+			</div>
+		</Col>
+		<Col sm="4" xs="12">     
+		<div className="footercategory">
+			<List type="unstyled">
+				<li>
+					<a href="#">About us</a>
+				</li>
+				<li>
+					<a href="#">Contact us</a>
+				</li>
+				<li>
+					<a href="#">Customer Service</a>
+				</li>
+				<li>
+					<a href="#">Technical Support</a>
+				</li>
+				<li>
+					<a href="#">Sales</a>
+				</li>
+			</List>
+		</div>
+	</Col>
+	<Col sm="4" xs="12">     
+	<div className="footercategory">
+		<List type="unstyled">
+			<li>
+				<a href="#">Privacy Policy</a>
+			</li>
+			<li>
+				<a href="#">Return Policy</a>
+			</li>
+			<li>
+				<a href="#">Terms of Service</a>
+			</li>
+			<li>
+				<a href="#">Merchant Signup</a>
+			</li>
+		</List>
+	</div>
+</Col>
+</Row>
+</Container>
+</div>
+);
 }
 
 export default Footer;
