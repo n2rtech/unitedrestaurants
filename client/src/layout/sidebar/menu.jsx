704bff3538193376bda31dbf6e1745e44837ae19
Sohrab <sohrab@n2rtechnologies.com>
2021-11-17 16:41:42 +0530

    Update menu.jsx

--- client/src/layout/sidebar/menu.jsx
+++ client/src/layout/sidebar/menu.jsx
@@ -6,37 +6,37 @@ export const MENUITEMS = [
         menucontent:"Dashboards,Widgets",
         Items:[
             {
-                title: 'Dashboard', icon: Home, path: `${process.env.PUBLIC_URL}/dashboard/default`, type: 'link', active: true,
+                title: 'Dashboard', icon: Home, path: `${process.env.PUBLIC_URL}/dashboard/default`, type: 'link', active: false,
             }
         ]
     },
 
     {
-        menutitle:"Manage Roles",
-        menucontent:"Manage Roles",
+        menutitle:"Roles",
+        menucontent:"Roles",
         Items:[
             {
-                title: 'Manage Roles', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false,
+                title: 'Roles', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false,
             }
         ]
     },
 
     {
-        menutitle:"Manage Permission",
-        menucontent:"Manage Permission",
+        menutitle:"Permission",
+        menucontent:"Permission",
         Items:[
             {
-                title: 'Manage Permission', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false,
+                title: 'Permission', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false,
             }
         ]
     },
     
     {
-        menutitle:"Manage Categories",
-        menucontent:"Manage Categories",
+        menutitle:"Categories",
+        menucontent:"Categories",
         Items:[
             {
-                title: 'Manage Categories', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false, 
+                title: 'Categories', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false, 
             },
 
         ]
@@ -44,18 +44,105 @@ export const MENUITEMS = [
 
     {
         menutitle:"Vendors",
-        menucontent:"Manage Vendors",
+        menucontent:"Vendors",
         Items:[
             {
-                title: 'Manage Membership packages', icon: Layers, type: 'sub', badge2: true, active: false, children: [
-                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'Sample Page', type: 'link' },  
-                    { path: `${process.env.PUBLIC_URL}/pages/searchpage`, title: 'Search Pages', type: 'link' },
+                title: 'Vendors', icon: Layers, path: `${process.env.PUBLIC_URL}/dashboard/default`, type: 'sub', active: false, children: [
+                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'All membership vendors', type: 'link' },  
+                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'No membership vendors', type: 'link' },  
+                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'Suspended vendors', type: 'link' },  
+                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'Featured Vendors', type: 'link' },  
+                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'Hot deals vendors', type: 'link' },  
+                    { path: `${process.env.PUBLIC_URL}/pages/searchpage`, title: 'Add spaces vendors', type: 'link' },
         
             ]
-            },
-            {
-                
             }
         ]
-    }
+    },
+    {
+        menutitle:"Merchants",
+        menucontent:"Merchants",
+        Items:[
+            {
+                title: 'Merchants', icon: Layers, path: `${process.env.PUBLIC_URL}/dashboard/default`, type: 'sub', active: false, children: [
+                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'Add a merchant', type: 'link' },
+                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'All merchants', type: 'link' },  
+                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'Subscribed merchants', type: 'link' },  
+                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'Video membership merchants', type: 'link' },  
+                    { path: `${process.env.PUBLIC_URL}/pages/samplepage`, title: 'Suspended merchants', type: 'link' },              
+        
+            ]
+            }
+        ]
+    },
+    {
+        menutitle:"Membership packages",
+        menucontent:"Membership packages",
+        Items:[
+            {
+                title: 'Membership packages', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false, 
+            },
+
+        ]
+    },
+    {
+        menutitle:"Notifications & messaging",
+        menucontent:"Notifications & messaging",
+        Items:[
+            {
+                title: 'Notifications & messaging', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false, 
+            },
+
+        ]
+    },
+    {
+        menutitle:"Promotions/discounts",
+        menucontent:"Promotions/discounts",
+        Items:[
+            {
+                title: 'Promotions/discounts', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false, 
+            },
+
+        ]
+    },
+    {
+        menutitle:"Accounts Payable",
+        menucontent:"Accounts Payable",
+        Items:[
+            {
+                title: 'Accounts Payable', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false, 
+            },
+
+        ]
+    },
+    {
+        menutitle:"Countries",
+        menucontent:"Countries",
+        Items:[
+            {
+                title: 'Countries', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false, 
+            },
+
+        ]
+    },
+    {
+        menutitle:"Google Adsense",
+        menucontent:"Google Adsense",
+        Items:[
+            {
+                title: 'Google Adsense', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false, 
+            },
+
+        ]
+    },
+    {
+        menutitle:"Site settings",
+        menucontent:"Site settings",
+        Items:[
+            {
+                title: 'Site settings', path: `${process.env.PUBLIC_URL}/dashboard/default`, icon: Users, type: 'link', active: false, 
+            },
+
+        ]
+    },
 ]
