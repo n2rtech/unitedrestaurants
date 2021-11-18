5cbfa67c7caa30331f24e0b517179b23c3738905
krishna mishra <er.krishna.mishra@gmail.com>
2021-11-18 15:54:15 +0530

    get user by role id

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,6 +7,11 @@ const keys = require("../../config/keys");
 const nodemailer = require("nodemailer");
 const User = require('../../models').User;
 const Role = require('../../models').Role;
+const Category = require('../../models').Category;
+const passport = require('passport');
+require('../../config/passport')(passport);
+const Helper = require('../../utils/helper');
+const helper = new Helper();
 
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
@@ -106,10 +111,117 @@ router.post("/login", (req, res) => {
       });
     }
   });
-
-
-
 });
+
+
+
+// Get Users by Role ID
+router.get('/by-role/:id', passport.authenticate('jwt', {
+  session: false
+}), function (req, res) {
+  helper.checkPermission(req.user.role_id, 'role_add').then((rolePerm) => {
+
+  }).catch((error) => {
+    res.status(403).send(error);
+  });
+  Role.findByPk(req.params.id)
+  .then((role) => {            
+    if (role) {
+      User.findAll({
+        where: {
+          role_id: role.id
+        }
+      }).then((users) => {
+        res.status(200).send(
+          users
+          );
+      }).catch(err => res.status(400).send(err));
+    } else {
+      res.status(404).send({
+        'message': 'Role not found'
+      });
+    }
+  })
+  .catch((error) => {
+    res.status(400).send(error);
+  });
+});
+
+
+// Get User by ID
+router.get('/:id', passport.authenticate('jwt', {
+  session: false
+}), function (req, res) {
+  helper.checkPermission(req.user.role_id, 'role_add').then((rolePerm) => {
+    User
+      .findByPk(req.params.id)
+      .then((user) => res.status(200).send(user))
+      .catch((error) => {
+        res.status(400).send(error);
+      });
+  }).catch((error) => {
+    res.status(403).send(error);
+  });
+});
+
+
+
+
+// Update a User
+router.put('/:id', passport.authenticate('jwt', {
+  session: false
+}), function (req, res) {
+  helper.checkPermission(req.user.role_id, 'role_add').then((rolePerm) => {
+    if (!req.body.role_id || !req.body.email || !req.body.password || !req.body.fullname || !req.body.phone) {
+      res.status(400).send({
+        msg: 'Please pass Role ID, email, password, phone or fullname.'
+      })
+    } else {
+      User
+        .findByPk(req.params.id)
+        .then((user) => {
+
+
+          if (req.body.password) {
+            var password = generatePassword(req.body.password);
+          }else{
+            var password = user.password;
+          }
+
+          User.update({
+            email: req.body.email || user.email,
+            name: req.body.name || user.name,
+            phone: req.body.phone || user.phone,
+            mobile: req.body.mobile || user.mobile,
+            password: password,
+            role_id: req.body.role_id || user.role_id,
+            address: req.body.address || user.address,
+          }, {
+            where: {
+              id: req.params.id
+            }
+          }).then(_ => {
+            res.status(200).send({
+              'message': 'User updated'
+            });
+          }).catch(err => res.status(400).send(err));
+        })
+        .catch((error) => {
+          res.status(400).send(error);
+        });
+    }
+  }).catch((error) => {
+    res.status(403).send(error);
+  });
+});
+
+
+function generatePassword(pwd) {
+    let hash = bcrypt.hashSync(pwd, 10);
+    return hash;
+};
+
+
 
 module.exports = router;
   
