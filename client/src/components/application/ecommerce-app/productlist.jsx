165c4a3a5b1e3a153e315d269a64d22c7b562c2d
ganesh <nganesh@n2rtechnologies.in>
2021-11-19 12:30:31 +0530

    edit , delete button added in permission list page

--- client/src/components/application/ecommerce-app/productlist.jsx
+++ client/src/components/application/ecommerce-app/productlist.jsx
@@ -1,11 +1,6 @@
 import React, { Fragment , useState , useEffect } from 'react';
 import Breadcrumb from '../../../layout/breadcrumb'
-import {Container,Row,Col,Card,CardHeader,CardBody , Table} from 'reactstrap'
-import DataTable from 'react-data-table-component';
-import {productData,productColumns} from '../../../data/product-list'
-import { ProductListTitle, ProductListDesc } from '../../../constant';
-import axios from 'axios'
-
+import {Container,Row,Col,Card,Button,CardBody,Table} from 'reactstrap'
 
 const Productlist = () => {
 
@@ -31,6 +26,31 @@ useEffect(() => {
       )
   }, []);
 
+  const handleDelete = () => {
+        
+    if (window.confirm(`Are you sure you want to delete:`)) {
+        
+    }
+  };
+
+  const handleEdit = () => {
+      
+        
+    // const config = {
+    //     headers: { 'Content-Type': 'application/json'  ,'Access-Control-Allow-Origin': '*' , 'Authorization': 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IktyaXNobmEgTWlzaHJhIiwiZW1haWwiOiJrcmlzaG5hQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYzNzEyNTI5NSwiZXhwIjoxNjY4NjgyMjIxfQ.XQnBPN7Vc1zahxytp0YiGQG9DUOs7SU94tFtEvQiX78' }
+    //     };
+    //     const bodyParameters = {
+    //       perm_name: pername,
+    //       perm_description : perdesc
+    //     };
+    //     axios.put(`/api/permissions`,
+    //       bodyParameters,
+    //       config
+    //     ) .then(response => console.log('Submiited Successfully'))
+    //        .catch(error => console.log('Form submit error', error))
+  };
+
+
 console.log(generalData);
 
     return (
@@ -50,6 +70,7 @@ console.log(generalData);
                                             <th scope="col">{"Last Name"}</th>
                                             <th scope="col">{"Created at"}</th>
                                             <th scope="col">{"Updated at"}</th>
+                                            <th scope="col">{"Action"}</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -60,6 +81,10 @@ console.log(generalData);
                                               <th scope="row">{item.perm_description}</th>
                                               <th scope="row">{item.createdAt}</th>
                                               <th scope="row">{item.updatedAt}</th>
+                                              <th scope="row">
+                                                    <Button color="primary" size="sm" onClick={handleEdit}><i className="fa fa-pencil"></i>Edit</Button>
+                                                    <Button color="danger" size="sm" onClick={handleDelete}><i className="fa fa-trash"></i> Delete</Button>
+                                              </th>
                                          </tr>
                                         ))}
                                     </tbody>
