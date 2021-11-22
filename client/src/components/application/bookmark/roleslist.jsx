b821417e0e6d2f3ea3a64ded3d26bdfb501b8de6
Sohrab <sohrab@n2rtechnologies.com>
2021-11-22 12:35:12 +0530

    Selectpicker added

--- client/src/components/application/bookmark/roleslist.jsx
+++ client/src/components/application/bookmark/roleslist.jsx
@@ -1,12 +1,20 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Breadcrumb from '../../../layout/breadcrumb'
 import { Container, Row, Col, Card, CardBody, CardHeader, Nav, NavItem, TabContent, TabPane, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Button } from 'reactstrap'
 import { Grid, List, Link, Share2, Trash2, Tag, Edit2, Bookmark, PlusCircle } from 'react-feather';
 import { useForm } from 'react-hook-form'
-import defaultImg from '../../../assets/images/lightgallry/01.jpg'
+import { Typeahead } from 'react-bootstrap-typeahead';
+import TypeaheadOne from './typeahead-one';
+import axios from 'axios'
 import { useSelector, useDispatch } from 'react-redux'
 
 const RolesList = (props) => {
+const multiple = false
+    const [options,setOptions] = useState([])
+
+    useEffect(() => {
+        axios.get(`${process.env.PUBLIC_URL}/api/typeaheadData.json`).then(res => setOptions(res.data))
+    },[])
 
   return (
     <Fragment>
@@ -18,7 +26,15 @@ const RolesList = (props) => {
             <h1>Role Name</h1>
             <span>Accounts Manager</span>
             <h2>Permissions</h2>
-
+            <Typeahead
+              id="multiple-typeahead"
+              clearButton
+              defaultSelected={options.slice(0, 5)}
+              labelKey={"name"}
+              multiple
+              options={options}
+              placeholder="Choose a state..."
+            />
             <div>&nbsp;</div>
             <Button color="primary">Submit</Button>
           </div>
