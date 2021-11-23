cc0c570798f55a9b0936ee61db4c342eee64b87d
ganesh <nganesh@n2rtechnologies.in>
2021-11-23 13:29:51 +0530

    categories list with edit

--- client/src/components/application/bookmark/edit-category.jsx
+++ client/src/components/application/bookmark/edit-category.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect , useState } from 'react';
 import Breadcrumb from '../../../layout/breadcrumb'
 import { Table, Container, Row, Col, Card, CardBody, CardHeader, Nav, NavItem, TabContent, TabPane, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Button } from 'reactstrap'
 import { Grid, List, Link, Share2, Trash2, Tag, Edit2, Bookmark, PlusCircle } from 'react-feather';
@@ -6,10 +6,12 @@ import { useForm } from 'react-hook-form'
 import { useSelector, useDispatch } from 'react-redux'
 import ImageUploader from 'react-images-upload';
 import {SelectSingleImageUpload,MultipleImageUpload} from '../../../constant'
-
+import { useParams } from "react-router-dom";
 
 
 const EditCategory = (props) => {
+
+const params = useParams();
 
 const [image, setimage] = useState({ pictures: [] })
 
@@ -18,6 +20,30 @@ const [image, setimage] = useState({ pictures: [] })
             ...image, pictureFiles
         });
     }
+
+    const [generalData, setGeneralData] = useState([]);
+
+    useEffect(() => {
+    
+        const config = {
+            headers: { 'Content-Type': 'application/json'  ,'Access-Control-Allow-Origin': '*' , 'Authorization': 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IktyaXNobmEgTWlzaHJhIiwiZW1haWwiOiJrcmlzaG5hQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYzNzEyNTI5NSwiZXhwIjoxNjY4NjgyMjIxfQ.XQnBPN7Vc1zahxytp0YiGQG9DUOs7SU94tFtEvQiX78' }
+            };
+       
+        fetch("/api/categories/"+`${params.id}` , config)
+          .then(res => res.json())
+          .then(
+            (result) => {
+              
+                setGeneralData(result);
+            },
+            (error) => {
+              
+            }
+          )
+      }, []);
+
+      const cat = generalData.name;
+      console.log(generalData);
 
   return (
     <Fragment>
@@ -28,7 +54,7 @@ const [image, setimage] = useState({ pictures: [] })
           <Form className="form theme-form">
             <FormGroup>
               <Label htmlFor="exampleFormControlInput1">{"Category Name"}</Label>
-              <Input className="form-control"  type="name" placeholder="Restaurant" />
+              <Input className="form-control" value= {cat} type="name" placeholder={generalData.name} />
             </FormGroup>
             <FormGroup>
               <ImageUploader
