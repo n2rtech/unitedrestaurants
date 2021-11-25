d22185a1d050bd4993ba3aa8c11941449fd1ad00
ganesh <nganesh@n2rtechnologies.in>
2021-11-25 13:14:51 +0530

    Delete job openings task

--- client/src/components/application/bookmark/job-openings.jsx
+++ client/src/components/application/bookmark/job-openings.jsx
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect , useState } from 'react';
 import Breadcrumb from '../../../layout/breadcrumb'
 import { Table, Container, Row, Col, Card, CardBody, CardHeader, Nav, NavItem, TabContent, TabPane, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Button } from 'reactstrap'
 import axios from 'axios'
+import {toast} from 'react-toastify';
 
 const JobOpenings = (props) => {
 
@@ -25,6 +26,19 @@ const JobOpenings = (props) => {
           }
         )
     }, []);
+
+    // Delete functionality
+
+    const handleDelete = (id) => {
+      const config = {
+        headers: { 'Content-Type': 'application/json'  ,'Access-Control-Allow-Origin': '*' , 'Authorization': 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IktyaXNobmEgTWlzaHJhIiwiZW1haWwiOiJrcmlzaG5hQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYzNzEyNTI5NSwiZXhwIjoxNjY4NjgyMjIxfQ.XQnBPN7Vc1zahxytp0YiGQG9DUOs7SU94tFtEvQiX78' }
+        };
+
+        axios.delete(`/api/jobs/`+`${id}`,config
+        ) .then(response => toast.success("Deleted Jobs !")  )
+           .catch(error => console.log('Form submit error', error))
+      
+    }
 
 
   return (
@@ -55,7 +69,7 @@ const JobOpenings = (props) => {
                     <td>{item.job_name}</td>
                     <td className="text-right">
                       <a href={`${process.env.PUBLIC_URL}/dashboard/vendor/edit-job-openings/`+`${item.id}`} className="btn btn-success">Edit</a> &nbsp;
-                      <a href={"#"} className="btn btn-danger">Delete</a> 
+                      <a href={"#"} className="btn btn-danger" onClick={() => handleDelete(item.id)}>Delete</a> 
                     </td>
                   </tr>
                   ))}
