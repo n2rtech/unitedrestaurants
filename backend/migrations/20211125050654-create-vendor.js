748a2ca6d3d17e4beac264c0233553157f09721c
krishna mishra <er.krishna.mishra@gmail.com>
2021-11-25 10:43:03 +0530

    profile update api

---
+++ backend/migrations/20211125050654-create-vendor.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Vendors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      country_id: {
+        type: Sequelize.INTEGER
+      },
+      category_id: {
+        type: Sequelize.INTEGER
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      department: {
+        type: Sequelize.STRING
+      },
+      subscription_plan: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      membership_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Vendors');
+  }
+};
---
+++ backend/models/vendor.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Vendor extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Vendor.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    country_id: DataTypes.INTEGER,
+    category_id: DataTypes.INTEGER,
+    country: DataTypes.STRING,
+    department: DataTypes.STRING,
+    subscription_plan: DataTypes.STRING,
+    status: DataTypes.INTEGER,
+    membership_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Vendor',
+  });
+  return Vendor;
+};
