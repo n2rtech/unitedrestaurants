44bf3898e8566d9c7c14424735dea477e8bc0722
ganesh <nganesh@n2rtechnologies.in>
2021-11-29 17:26:31 +0530

    menu-items edit updated

--- client/src/components/application/bookmark/menu-items.jsx
+++ client/src/components/application/bookmark/menu-items.jsx
@@ -1,9 +1,10 @@
-import React, { Fragment , useState } from 'react';
+import React, { Fragment , useEffect , useState } from 'react';
 import Breadcrumb from '../../../layout/breadcrumb'
 import CKEditors from "react-ckeditor-component";
 import {Container,Row,Col,Card,CardHeader,CardBody, Button} from 'reactstrap'
 import {toast} from 'react-toastify';
 import axios from 'axios'
+import { useParams } from "react-router-dom";
 import { useHistory } from 'react-router-dom'
 
 const MenuItems =  () =>  {
@@ -13,6 +14,52 @@ const MenuItems =  () =>  {
         const newContent = evt.editor.getData();
         setContent(newContent)
     }
+
+    const params = useParams();
+    const token = localStorage.getItem("token");
+    const history = useHistory()
+    useEffect(() => {
+    
+        const config = {
+            headers: { 'Content-Type': 'application/json'  ,'Access-Control-Allow-Origin': '*' , 'Authorization': 'JWT '+token }
+        };
+     
+        fetch("/api/menu-items/"+`${params.id}` , config)
+          .then(res => res.json())
+          .then(
+            (result) => {
+                setContent(result.content);
+            },
+            (error) => {
+              
+            }
+          )
+      }, []);
+  
+      console.log(content);
+
+  // Edit Api
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const config = {
+      headers: { 'Content-Type': 'application/json'  ,'Access-Control-Allow-Origin': '*' , 'Authorization': 'JWT '+token }
+      };
+      const bodyParameters = {
+        content: content,
+      };
+      axios.put(`/api/menu-items/`+`${params.id}`,
+        bodyParameters,
+        config
+      ) .then(response => {
+        toast.success("Menu Items updated !")
+          setTimeout(() => {
+            history.push('/dashboard/vendor/menu-items/1');
+          }, 1000);
+      })
+         .catch(error => console.log('Form submit error', error))
+
+  };
 
     return (
             <Fragment>
@@ -33,7 +80,7 @@ const MenuItems =  () =>  {
                                         }}
                                     />
                                     <div className="m-t-20">
-                                    <Button color="primary">{"Save"}</Button>
+                                    <Button color="primary" onClick = {handleSubmit} >{"Save"}</Button>
                                     </div>
                                 </CardBody>
                             </Card>
