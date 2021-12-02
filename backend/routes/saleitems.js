b9b46ac0ca6822587a81491c0f219951a89a1526
krishna mishra <er.krishna.mishra@gmail.com>
2021-12-02 19:34:21 +0530

    apis to get data for front end details page

---
+++ backend/routes/saleitems.js
@@ -0,0 +1,145 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models').User;
+const Role = require('../models').Role;
+const Permission = require('../models').Permission;
+const SaleItem = require('../models').SaleItem;
+const passport = require('passport');
+require('../config/passport')(passport);
+const Helper = require('../utils/helper');
+const helper = new Helper();
+
+// Create a new SaleItem
+router.post('/', (req, res) => {
+        if (!req.body.deal_name || !req.body.deal_description) {
+            res.status(400).send({
+                msg: 'Please pass saleitem name or description.'
+            })
+        } else {
+            SaleItem
+                .create({
+                    deal_name: req.body.deal_name,
+                    user_id: req.body.user_id,
+                    deal_description: req.body.deal_description
+                })
+                .then((saleitem) => res.status(201).send(saleitem))
+                .catch((error) => {
+                    console.log(error);
+                    res.status(400).send(error);
+                });
+        }
+});
+
+// Get List of SaleItems
+
+
+router.get('/list', (req, res) => {
+        SaleItem
+            .findAll()
+            .then((saleitem) => res.status(200).send(saleitem))
+            .catch((error) => {
+                res.status(400).send(error);
+            });
+});
+
+
+router.get('/', passport.authenticate('jwt', {
+    session: false
+}), function (req, res) {   
+    SaleItem
+    .findAll({where:{user_id:req.user.id}})
+    .then((saleitem) => res.status(200).send(saleitem))
+    .catch((error) => {
+        res.status(400).send(error);
+    });
+});
+
+
+
+router.get('/list/:id', (req, res) => {   
+    SaleItem
+    .findAll({where:{user_id:req.params.id}})
+    .then((saleitem) => res.status(200).send(saleitem))
+    .catch((error) => {
+        res.status(400).send(error);
+    });
+});
+
+
+
+
+// Get SaleItem by ID
+router.get('/:id', (req, res) => {
+    SaleItem
+        .findOne({
+            where:{
+           user_id : req.params.id
+        }})
+        .then((saleitem) => res.status(200).send(saleitem))
+        .catch((error) => {
+            res.status(400).send(error);
+        });
+});
+
+// Update a Menu Items
+router.put('/:id', function (req, res) {
+        if (!req.params.id || !req.body.content) {
+            res.status(400).send({
+                msg: 'Please pass Job ID, description.'
+            })
+        } else {
+            SaleItem
+                .findByPk(req.params.id)
+                .then((saleitem) => {
+                    SaleItem.update({
+                        user_id: req.body.user_id || saleitem.user_id,
+                        content: req.body.content || saleitem.content
+                    }, {
+                        where: {
+                            id: req.params.id
+                        }
+                    }).then(_ => {
+                        res.status(200).send({
+                            'message': 'Menu Item updated'
+                        });
+                    }).catch(err => res.status(400).send(err));
+                })
+                .catch((error) => {
+                    res.status(400).send(error);
+                });
+        }
+});
+
+// Delete a SaleItem
+router.delete('/:id', (req, res) => {
+        if (!req.params.id) {
+            res.status(400).send({
+                msg: 'Please pass saleitem ID.'
+            })
+        } else {
+            SaleItem
+                .findByPk(req.params.id)
+                .then((saleitem) => {
+                    if (saleitem) {
+                        SaleItem.destroy({
+                            where: {
+                                id: req.params.id
+                            }
+                        }).then(_ => {
+                            res.status(200).send({
+                                'message': 'Job deleted'
+                            });
+                        }).catch(err => res.status(400).send(err));
+                    } else {
+                        res.status(404).send({
+                            'message': 'Job not found'
+                        });
+                    }
+                })
+                .catch((error) => {
+                    res.status(400).send(error);
+                });
+        }
+});
+
+module.exports = router;
