6ae32e57bc2f5f0a635b3e12936a88b9911bdc69
ganesh <nganesh@n2rtechnologies.in>
2021-12-06 11:29:11 +0530

    adds memberships in vendor

--- client/src/components/application/bookmark/adds-membership.jsx
+++ client/src/components/application/bookmark/adds-membership.jsx
@@ -18,7 +18,7 @@ const AddsMembership = (props) => {
           headers: { 'Content-Type': 'application/json'  ,'Access-Control-Allow-Origin': '*' , 'Authorization': 'JWT '+token }
           };
    
-      fetch("/api/membership" , config)
+      fetch("/api/adds-membership" , config)
         .then(res => res.json())
         .then(
           (result) => {
@@ -41,11 +41,12 @@ const AddsMembership = (props) => {
               <Col sm="12">
                 <Card>
                   <Row className="card-body pricing-content AddsMembership">
-                    <Col xl="4" sm="4" className="box-col-6">
+                  {membershipsData.map((item , i) => (
+                    <Col  key={i}  xl="4" sm="4" className="box-col-6">
                       <Card className="text-center pricing-simple">
                         <CardBody>
-                          <h3>{"Bronze"}</h3>
-                          <h1>{"$4.99"}<span className="duration">{"/mo"}</span></h1>
+                          <h3>{item.name}</h3>
+                          <h1>{item.price}<span className="duration">{"/mo"}</span></h1>
                           <h6 className="mb-0">{"Your ad will appear in all sections you are listed in in the country your business resides."}</h6>
                           <FormGroup className="text-left m-t-20">
                             <Label htmlFor="exampleFormControlSelect9">{"Select your plan"}</Label>
@@ -60,44 +61,7 @@ const AddsMembership = (props) => {
                           <h5 className="mb-0">{Purchase}</h5></Button>
                       </Card>
                     </Col>
-                    <Col xl="4" sm="4" className="box-col-6">
-                      <Card className="text-center pricing-simple">
-                        <CardBody>
-                          <h3>{"Silver"}</h3>
-                          <h1>{"$6.99"}<span className="duration">{"/mo"}</span></h1>
-                          <h6 className="mb-0">{"All what is included in Bronze package +  Your ad will appear on homepage"}</h6>
-                          <FormGroup className="text-left m-t-20">
-                            <Label htmlFor="exampleFormControlSelect9">{"Select your plan"}</Label>
-                            <Input type="select" name="select" className="form-control digits" defaultValue="1">
-                              <option>{"Monthly"}</option>
-                              <option>{"Quarterly"}</option>
-                              <option>{"Half Yearly"}</option>
-                              <option>{"Yearly"}</option>
-                            </Input>
-                          </FormGroup>
-                        </CardBody><Button className="btn-block" size="lg" color="primary">
-                          <h5 className="mb-0">{Purchase}</h5></Button>
-                      </Card>
-                    </Col>
-                    <Col xl="4" sm="4" className="box-col-6">
-                      <Card className="text-center pricing-simple">
-                        <CardBody>
-                          <h3>{"Gold"}</h3>
-                          <h1>{"$9.99"}<span className="duration">{"/mo"}</span></h1>
-                          <h6 className="mb-0">{"All what is included in Bronze and Silver package +  Your ad will appear on homepage, every departments all over worldwide"}</h6>
-                          <FormGroup className="text-left m-t-20">
-                            <Label htmlFor="exampleFormControlSelect9">{"Select your plan"}</Label>
-                            <Input type="select" name="select" className="form-control digits" defaultValue="1">
-                              <option>{"Monthly"}</option>
-                              <option>{"Quarterly"}</option>
-                              <option>{"Half Yearly"}</option>
-                              <option>{"Yearly"}</option>
-                            </Input>
-                          </FormGroup>
-                        </CardBody><Button className="btn-block" size="lg" color="primary">
-                          <h5 className="mb-0">{Purchase}</h5></Button>
-                      </Card>
-                    </Col>
+                  ))}
                   </Row>
                 </Card>
               </Col>
