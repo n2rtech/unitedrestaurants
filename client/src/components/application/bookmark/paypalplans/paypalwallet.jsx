4731ec8124c013cfec150c523c5e7fcbc05cf03d
ganesh <nganesh@n2rtechnologies.in>
2021-12-14 13:26:28 +0530

    wallet added money functionality done

--- client/src/components/application/bookmark/paypalplans/paypalwallet.jsx
+++ client/src/components/application/bookmark/paypalplans/paypalwallet.jsx
@@ -1,11 +1,13 @@
 import { PayPalButton } from "react-paypal-button-v2";
 import React from 'react';
+import axios from 'axios';
+import {toast} from 'react-toastify';
 
 const Paypalwallet = (props) => {
   const { amount, currency } = props;
   const paypalKey = "AYVHVHUfQuFd3o0L6bYflBZwU6KbCURHbh7ROB7Np68bsWwe27tqMhGglLoBLrNmjnc_SrKWHprxAnUK"
 
-  console.log(props);
+  console.log(props.amount);
 
   return (
     <PayPalButton
@@ -15,7 +17,7 @@ const Paypalwallet = (props) => {
         return details.order.create({
           purchase_units: [{
             amount: {
-              value: '0.01'
+              value: props.amount
             }
           }]
       });
@@ -25,10 +27,29 @@ const Paypalwallet = (props) => {
           return details.order.capture().then(function(details) {
             // Show a success message to your buyer
             console.log('Details' , details);
+            // console.log("Amount2",details.purchase_units[0].amount.value)
+            // console.log("Amount1" , details.purchase_units.amount.value);
             // details.5TB18426XL472184P = id
             // details.intent = CAPTURE
             // details.status = COMPLETED
-            alert('Transaction completed by ' + details.payer.name.given_name);
+            const token = localStorage.getItem("token");
+            const user_id = localStorage.getItem("id");
+            const config = {
+              headers: { 'Content-Type': 'application/json'  ,'Access-Control-Allow-Origin': '*' , 'Authorization': 'JWT '+token }
+              };
+
+              const bodyParameters = {
+                user_id: user_id,
+                transaction_id: details.id,
+                comment: "Added to wallet",
+                type: "deposit",
+                amount: details.purchase_units[0].amount.value
+              }
+
+            return axios.post('/api/wallet', bodyParameters ,config )
+            .then(response => toast.success('Transaction completed by ' + details.payer.name.given_name))
+            .catch(error => console.log('Form submit error', error))
+
 
           });
         }
