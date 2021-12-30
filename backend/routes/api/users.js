22637db68644da36c400e99d45e5e1e89d0a0b6a
ganesh <nganesh@n2rtechnologies.in>
2021-12-30 10:45:21 +0530

    password field added in manage user edit page

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -363,7 +363,7 @@ router.get('/:id', (req, res) => {
 
 // Update a User
 router.put('/:id', (req, res) => {
-    if (!req.body.role_id || !req.body.email || !req.body.name || !req.body.phone) {
+    if (!req.body.role_id || !req.body.email || !req.body.name) {
       res.status(400).send({
         msg: 'Please pass Role ID, email, phone or fullname.'
       })
