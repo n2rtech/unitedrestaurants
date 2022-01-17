52a5b8b6d46ab1b8ab0ff4777279d5a055c52ee6
ganesh <nganesh@n2rtechnologies.in>
2022-01-17 13:30:48 +0530

    restuarant categortwise

--- client/src/website/menu.jsx
+++ client/src/website/menu.jsx
@@ -16,18 +16,11 @@ const addDefaultSrc = (ev) => {
 }
 
 useEffect(() => {
-  //    axios.get(`/api/categories/list`)
-  // .then((result_data) => {
-  //   const result = result_data.data;
-  //   setCategoryData(result);
-  // }); 
-
   axios.get(`/api/categories/top-menu`)
     .then((result_data) => {
       const result = result_data.data;
       setCategoryData(result);
       for (const [i, element] of result.entries()) {
-        
         result[i].parent = [];
         var parent = element.id;
         if(parent){
@@ -82,212 +75,39 @@ console.log('Menu' , categoryData);
               <Collapse isOpen={isOpen} navbar>
                 <Nav
                   className="me-auto"
-                  navbar
-                >
-
-
-
+                  navbar>
                     {categoryData.map((item , i) => (
                       <NavItem key={i}>
-                            <img onError = {addDefaultSrc} src={`${process.env.PUBLIC_URL}/api/uploads/categories/${item.image}`} alt="Menu-Icon"/>
+                          <a href={`${process.env.PUBLIC_URL}/restaurants/${item.id}`}>  <img onError = {addDefaultSrc} src={`${process.env.PUBLIC_URL}/api/uploads/categories/${item.image}`} alt="Menu-Icon"/> </a>
                       <Dropdown title={item.name} key={i}>
-                      {item.parent &&
-                        <Fragment>
-{(item.parent).map((item1 , i) => (
-  <Dropdown.Item>{item1.name}
-{item1.parent_2 &&
-                        <Fragment>
-  {(item1.parent_2).map((item2 , i) => (
-    <Dropdown.Submenu>
-      <Dropdown.Item>{item2.name}
-{item2.parent_3 &&
-                        <Fragment>
-      {(item2.parent_3).map((item3 , i) => (
-        <Dropdown.Submenu>
-          <Dropdown.Item><NavLink href="#">{item3.name}</NavLink></Dropdown.Item>
-        </Dropdown.Submenu>
-        ))}
-      </Fragment>
-    }
+                            {item.parent && (item.parent).map((item1 , i) => (<Fragment>
+                                  <Dropdown.Item>{item1.name}
+                                  {item1.parent_2 &&
+                                        <Fragment>
+                                          {(item1.parent_2).map((item2 , i) => (
+                                            <Dropdown.Submenu>
+                                              <Dropdown.Item>{item2.name}
+                                                  {item2.parent_3 &&
+                                                        <Fragment>
+                                                          {(item2.parent_3).map((item3 , i) => (
+                                                        <Dropdown.Submenu>
+                                                          <Dropdown.Item><NavLink href="#">{item3.name}</NavLink></Dropdown.Item>
+                                                        </Dropdown.Submenu>
+                                                        ))}
+                                                          </Fragment>
+                                                  }
 
-      </Dropdown.Item>   
-
-    </Dropdown.Submenu>
-    ))}
-  </Fragment>
-}
-    
-  </Dropdown.Item>
-  ))}
-</Fragment>
-}
-
-</Dropdown>
-</NavItem>
+                                                </Dropdown.Item>   
+                                              </Dropdown.Submenu>
+                                            ))}
+                                          </Fragment>
+                                    }
+                                  </Dropdown.Item>
+                                  </Fragment>
+                                  ))}
+                      </Dropdown>
+                    </NavItem>
 ))}
-
-
-                   {/* {categoryData.map((item , i) => (
-                          
-                          <NavItem key={i}>
-                            <img src={`${process.env.PUBLIC_URL}/assets/images/menuicon/restaurant_Ic.png`} alt="Menu-Icon"/>
-                             {item.parent_id == 0 ? 
-
-                               <Dropdown title={item.name} >
-                                <Dropdown.Item>
-                                {item.name}
-                                                         
-                              </Dropdown.Item>  
-                              </Dropdown>     
-                            
-                              :
-
-                              null
-                          
-                            }
-                            
-                          </NavItem>
-                      ))}*/}
-                
-                  {/* <NavItem>
-                      <img src={`${process.env.PUBLIC_URL}/assets/images/menuicon/restaurant_Ic.png`} 
-               alt="Menu-Icon"/>
-                      
-                      <Dropdown
-                    title='Restaurants'
-                  >
-                    <Dropdown.Item
-                    >
-                      3 Star
-                      <Dropdown.Submenu>
-                        <Dropdown.Item>
-                          Indian
-                          <Dropdown.Submenu>
-                        <Dropdown.Item>
-                        <NavLink href="#">
-                          Veg
-                        </NavLink>
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                        <NavLink href="#">
-                          Non Veg
-                        </NavLink>
-                        </Dropdown.Item>
-                      </Dropdown.Submenu>
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                        <NavLink href="#">
-                          Chinese
-                        </NavLink>
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                        <NavLink href="#">
-                          Italian
-                        </NavLink>
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                        <NavLink href="#">
-                          Syrian
-                        </NavLink>
-                        </Dropdown.Item>
-                      </Dropdown.Submenu>
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      5 Star
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      7 Star
-                    </Dropdown.Item>
-                  </Dropdown>
-                   
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="#">
-                      <img src={`${process.env.PUBLIC_URL}/assets/images/menuicon/Food_ic.png`}
-               alt="Menu-Icon"/>
-                      Food Markets
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="#">
-                      <img src={`${process.env.PUBLIC_URL}/assets/images/menuicon/beer_ic.png`}
-               alt="Menu-Icon"/>
-                      Beer & Alcohol
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="#">
-                      <img src={`${process.env.PUBLIC_URL}/assets/images/menuicon/services_ic.png`}
-               alt="Menu-Icon"/>
-                      Services
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="#">
-                      <img src={`${process.env.PUBLIC_URL}/assets/images/menuicon/supplires_ic.png`}
-               alt="Menu-Icon"/>
-                      Suppliers
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="#">
-                      <img src={`${process.env.PUBLIC_URL}/assets/images/menuicon/buy&sell_ic.png`}
-               alt="Menu-Icon"/>
-                      Buy & Sell
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="#">
-                      <img src={`${process.env.PUBLIC_URL}/assets/images/menuicon/jobs_ic.png`}
-               alt="Menu-Icon"/>
-                      Jobs
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="#">
-                      <img src={`${process.env.PUBLIC_URL}/assets/images/menuicon/video.png`}
-               alt="Menu-Icon"/>
-                      Videos
-                    </NavLink>
-                  </NavItem>
-                  <UncontrolledDropdown
-                    inNavbar
-                    nav
-                  >
-                    <DropdownToggle>
-                      <img src={`${process.env.PUBLIC_URL}/assets/images/menuicon/others_ic.png`}
-               alt="Menu-Icon"/>
-                    
-                      Others
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                      <DropdownItem>
-                        Option 1
-                      </DropdownItem>
-                      <DropdownItem>
-                        Option 2
-                      </DropdownItem>
-                      <DropdownItem divider />
-                      <DropdownItem>
-                        Reset
-                      </DropdownItem>
-                    </DropdownMenu>
-                    <DropdownMenu right>
-                      <DropdownItem>
-                        Option 1
-                      </DropdownItem>
-                      <DropdownItem>
-                        Option 2
-                      </DropdownItem>
-                      <DropdownItem divider />
-                      <DropdownItem>
-                        Reset
-                      </DropdownItem>
-                    </DropdownMenu>
-
-                  </UncontrolledDropdown> */}
-
-
                   
                 </Nav>
               </Collapse>
