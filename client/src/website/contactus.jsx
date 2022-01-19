6a45975b844c22a4e9f6a16ab3e3739fab75c7e0
Avinash <kavinash@n2rtechnologies.in>
2022-01-19 13:35:12 +0530

    contact form is updated

--- client/src/website/contactus.jsx
+++ client/src/website/contactus.jsx
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-import { Container, Row, Col, Navbar, NavbarBrand, NavbarToggler, Collapse, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText, Carousel, CarouselIndicators, CarouselItem, CarouselCaption, CarouselControl, Card, CardBody, CardTitle, CardSubtitle, CardText, List, ListInlineItem, Form, FormGroup, Input, InputGroup, select, option, Label, Button, NavItem, NavLink, Nav,TabContent,TabPane } from 'reactstrap'
+import { Container, Row, Col, Navbar, NavbarBrand, NavbarToggler, Collapse, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText, Carousel, CarouselIndicators, CarouselItem, CarouselCaption, CarouselControl, Card, CardBody, CardTitle, CardSubtitle, CardText, List, ListInlineItem, Form, FormGroup, Input, FormText, InputGroup, select, option, Label, Button, NavItem, NavLink, Nav,TabContent,TabPane } from 'reactstrap'
 import Header from './header.jsx'
 import Footer from './footer.jsx'
 import contactus from './contactus.jsx'
@@ -29,8 +29,30 @@ useEffect(() => {
               <img src={`${process.env.PUBLIC_URL}/assets/images/hr.png`} 
                              alt="Menu-Icon"/>
                </div>
-               <Row className="m-0">
-               <Col><div dangerouslySetInnerHTML={{ __html: contactUsData.body }} /></Col>
+               <Row>
+                 <Col sm="12">
+                   <div className="contactform">
+                     <Form>
+                      <FormGroup>
+                        <Label for="name">Name</Label>
+                        <Input type="email" name="name" id="name" />
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="email">Email</Label>
+                        <Input type="email" name="email" id="email" />
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="number">Phone Number</Label>
+                        <Input type="number" name="email" id="number" />
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="message">Message</Label>
+                        <Input type="textarea" rows="5" cols="5" name="text" id="exampleText" />
+                      </FormGroup>
+                      <Button>Submit</Button>
+                    </Form>
+                   </div>
+                 </Col>
                </Row>
           </Container>
           </div>
