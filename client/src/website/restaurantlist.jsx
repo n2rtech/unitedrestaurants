578fb1e698f9ef7fa5a272a0a4600c3e1a34210d
ganesh <nganesh@n2rtechnologies.in>
2022-01-20 11:43:25 +0530

    dfd

--- client/src/website/restaurantlist.jsx
+++ client/src/website/restaurantlist.jsx
@@ -63,7 +63,7 @@ const Restaurantlist = (props) => {
                   </Col>
                   </Row>
                   <CardText>
-                      {(item.about_business).substring(0, 140)}...
+                      {item.about_business}
                   </CardText>
                   <Button>
                     <a href={`${process.env.PUBLIC_URL}/resturent/details/${item.id}`}>
