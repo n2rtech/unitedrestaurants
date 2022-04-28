d0d52ad31d857e338ec782e63f04a7d991e8d217
Sohrab <website.design1008@gmail.com>
2022-04-28 12:33:47 +0530

    Vendor Login design

--- client/src/auth/usersignin.jsx
+++ client/src/auth/usersignin.jsx
@@ -102,74 +102,62 @@ const Logins = (props) => {
         <Row>
         <Col xs="12">     
           <div className="login-card">
-            <div>
-              <div>
-                <a className="logo" href="index.html">
-                  <img className="img-fluid for-light" src={require("../assets/images/logo/login.png")} alt=""/>
-                  <img className="img-fluid for-dark" src={require("../assets/images/logo/logo_dark.png")} alt=""/>
-                </a>
-              </div>
-              <div className="login-main login-tab"> 
-                <Nav className="border-tab flex-column" tabs style={{display:'none'}}>
-                  <NavItem>
-                    <NavLink className={selected === 'firebase' ? 'active' : ''} onClick={() => setSelected('firebase')}>
-                      <img src={require("../assets/images/firebase.svg")} alt="" />
-                      <span>{FIREBASE}</span>
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink className={selected === 'jwt' ? 'active' : ''} onClick={() => setSelected('jwt')}>
-                    <img src={require("../assets/images/jwt.svg")} alt="" />
-                    <span>{JWT}</span>
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink className={selected === 'auth0' ? 'active' : ''} onClick={() => setSelected('auth0')}>
-                    <img src={require("../assets/images/auth0.svg")} alt="" />
-                    <span>{AUTH0}</span>
-                    </NavLink>
-                  </NavItem>
-                </Nav>
-                <TabContent activeTab={selected} className="content-login">
-                  <TabPane  className="fade show" tabId={selected === "firebase" ? "firebase" : "jwt"}>
-                    <Form className="theme-form">
-                      <h4>{selected === "firebase" ? "Sign In With Firebase" : "Sign In for Admin or Vendor"}</h4>
-                      <p>{"Enter your email & password to login"}</p>
-                      <FormGroup>
-                        <Label className="col-form-label">{EmailAddress}</Label>
-                        <Input className="form-control" placeholder="Enter Email Address" type="email" required="" onChange={e => setEmail(e.target.value)} />
-                      </FormGroup>
-                      <FormGroup>
-                        <Label className="col-form-label">{Password}</Label>
-                        <Input className="form-control" autoComplete="new-password" placeholder="Enter Password" type={togglePassword ?  "text" : "password"} onChange={e => setPassword(e.target.value)} required=""/>
-                        <div className="show-hide" onClick={() => setTogglePassword(!togglePassword)}><span className={togglePassword ? "" : "show"}></span></div>
-                      </FormGroup>
-                      <div className="form-group mb-0">
-                        {/*<div className="checkbox ml-3">
-                          <Input id="checkbox1" type="checkbox"/>
-                          <Label className="text-muted" for="checkbox1">{RememberPassword}</Label>
-                        </div><a className="link" href="#javascript">{ForgotPassword}</a>*/}
-                        {selected === "firebase" ?
-                        <Button color="primary" className="btn-block" disabled={loading ? loading : loading} onClick={(e) => loginAuth(e)}>{loading ? "LOADING..." : SignIn }</Button>
-                        :
-                        <Button color="primary" className="btn-block" onClick={() => loginWithJwt(email,password)}>Login</Button>
-                        }
-                      </div>
-                      {/*<h6 className="text-muted mt-4 or">{"Or Sign in with"}</h6>*/}
-                      
-                      <p className="mt-4 mb-0">{"Don't have account?"}<a className="ml-2" href="/signup">{CreateAccount}</a></p>
-                    </Form>
-                  </TabPane>
-                  <TabPane  className="fade show" tabId="auth0">
-                    <div className="auth-content">
-                        <img src={require("../assets/images/auth-img.svg")} alt="" />
-                        <h4>{"Welcome to login with Auth0"}</h4>
-                        <p>{"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"}</p>
-                        <Button color="info" onClick={loginWithRedirect}>{AUTH0}</Button> 
+            <div className="login-main login-tab"> 
+              <Nav className="border-tab flex-column" tabs style={{display:'none'}}>
+                <NavItem>
+                  <NavLink className={selected === 'firebase' ? 'active' : ''} onClick={() => setSelected('firebase')}>
+                    <img src={require("../assets/images/firebase.svg")} alt="" />
+                    <span>{FIREBASE}</span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className={selected === 'jwt' ? 'active' : ''} onClick={() => setSelected('jwt')}>
+                  <img src={require("../assets/images/jwt.svg")} alt="" />
+                  <span>{JWT}</span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className={selected === 'auth0' ? 'active' : ''} onClick={() => setSelected('auth0')}>
+                  <img src={require("../assets/images/auth0.svg")} alt="" />
+                  <span>{AUTH0}</span>
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <TabContent activeTab={selected} className="content-login">
+                <TabPane  className="fade show" tabId={selected === "firebase" ? "firebase" : "jwt"}>
+                  <Form className="theme-form">
+                    <a className="logo" href="index.html">
+                      <img className="img-fluid for-light" src={require("../assets/images/logo/login.png")} alt=""/>
+                    </a>
+                    <h4>{selected === "firebase" ? "Sign In With Firebase" : "Vendor Login"}</h4>
+                    <FormGroup>
+                      <Label className="col-form-label">{EmailAddress}</Label>
+                      <Input className="form-control" placeholder="Enter Email Address" type="email" required="" onChange={e => setEmail(e.target.value)} />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label className="col-form-label">{Password}</Label>
+                      <Input className="form-control" autoComplete="new-password" placeholder="Enter Password" type={togglePassword ?  "text" : "password"} onChange={e => setPassword(e.target.value)} required=""/>
+                      <div className="show-hide" onClick={() => setTogglePassword(!togglePassword)}><span className={togglePassword ? "" : "show"}></span></div>
+                    </FormGroup>
+                    <div className="form-group signButton">
+                      {selected === "firebase" ?
+                      <Button color="danger" className="btn-block" disabled={loading ? loading : loading} onClick={(e) => loginAuth(e)}>{loading ? "LOADING..." : SignIn }</Button>
+                      :
+                      <Button color="danger" className="btn-block" onClick={() => loginWithJwt(email,password)}>Login</Button>
+                      }
                     </div>
-                  </TabPane>
-                </TabContent>
-              </div>
+                    <p className="mt-4 mb-0">{"Don't have account?"}<a className="ml-2" href="/signup">{CreateAccount}</a></p>
+                  </Form>
+                </TabPane>
+                <TabPane  className="fade show" tabId="auth0">
+                  <div className="auth-content">
+                      <img src={require("../assets/images/auth-img.svg")} alt="" />
+                      <h4>{"Welcome to login with Auth0"}</h4>
+                      <p>{"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"}</p>
+                      <Button color="info" onClick={loginWithRedirect}>{AUTH0}</Button> 
+                  </div>
+                </TabPane>
+              </TabContent>
             </div>
           </div>
         </Col>
