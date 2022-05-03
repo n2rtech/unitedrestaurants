ff02e42be7f391a3b927bc8765744cecd4fae331
ganesh <nganesh@n2rtechnologies.in>
2022-05-03 12:32:24 +0530

    memebership

--- client/src/components/application/bookmark/admin-membership-package.jsx
+++ client/src/components/application/bookmark/admin-membership-package.jsx
@@ -1,12 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Breadcrumb from '../../../layout/breadcrumb'
-import { Table, Container, Card, CardBody, Button, ButtonGroup } from 'reactstrap'
+import { Table, Container, Card, CardBody, ButtonGroup } from 'reactstrap'
 
 const AdminMembershipPackage = (props) => {
 
+  const token = localStorage.getItem("token");
+  const [membsershipPackage, setMembsershipPackage] = useState([]);
+  const [membersshipdata, setMembershipData] = useState([]);
+
+  useEffect(() => {
+
+    const config = {
+      headers: { 'Content-Type': 'application/json'  ,'Access-Control-Allow-Origin': '*' , 'Authorization': 'JWT '+token },
+    };
+
+    fetch("/api/membership" , config)
+    .then(res => res.json())
+    .then(
+      (result) => { 
+        setMembsershipPackage(result);  
+        setMembershipData(result);
+
+      },
+      (error) => { 
+      });
+  }, []);
+
+
+
   return (
     <Fragment>
-      <Breadcrumb parent="Apps" title="Add Membership Package" />
+      <Breadcrumb parent="Apps" title="Membership Packages" />
       <Container fluid={true}>
         <Card>
           <CardBody>
@@ -15,39 +39,28 @@ const AdminMembershipPackage = (props) => {
                 <thead>
                     <tr>
                         <th scope="col">{"Membership Name"}</th>
+                        <th scope="col">{"Description"}</th>
+                        <th scope="col">{"Membership Interval"}</th>
+                        <th scope="col">{"Membership Price"}</th>
                         <th scope="col" className="text-right">{"Action"}</th>
                     </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>{"Free"}</td>
-                    <td className="text-right">
-                    <ButtonGroup>
-                      <a className="btn btn-success" href={`${process.env.PUBLIC_URL}/dashboard/admin/edit-admin-membership/`}>Edit</a> &nbsp;
-                      <Button color="danger">Delete</Button>
-                    </ButtonGroup>
-                      
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>{"Standard"}</td>
-                    <td className="text-right">
-                    <ButtonGroup>
-                      <a className="btn btn-success" href={`${process.env.PUBLIC_URL}/dashboard/admin/edit-admin-membership/`}>Edit</a> &nbsp;
-                      <Button color="danger">Delete</Button>
-                    </ButtonGroup>
-                      
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>{"Premium"}</td>
-                    <td className="text-right">
-                    <ButtonGroup>
-                      <a className="btn btn-success" href={`${process.env.PUBLIC_URL}/dashboard/admin/edit-admin-membership/`}>Edit</a> &nbsp;
-                      <Button color="danger">Delete</Button>
-                    </ButtonGroup>
-                    </td>
-                  </tr>
+                  {membsershipPackage && membsershipPackage.map((item , i) => (
+                      <tr key={i}>
+                      <td>{item.name}</td>
+                      <td>{item.description}</td>
+                      <td>{item.interval}</td>
+                      <td>{item.price}</td>
+                      <td className="text-right">
+                      <ButtonGroup>
+                        <a className="btn btn-success" href={`${process.env.PUBLIC_URL}/dashboard/admin/edit-admin-membership/${item.id}/`}>Edit</a> &nbsp;
+                        {/* <Button color="danger">Delete</Button> */}
+                      </ButtonGroup>
+                        
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </Table>
             </div>
--- client/src/components/application/bookmark/edit-admin-membership.jsx
+++ client/src/components/application/bookmark/edit-admin-membership.jsx
@@ -1,15 +1,68 @@
-import React, { Fragment , useState } from 'react';
+import React, { Fragment, useEffect , useState } from 'react';
 import Breadcrumb from '../../../layout/breadcrumb'
 import CKEditors from "react-ckeditor-component";
 import {Container,Row,Col,Card,CardBody, Form, FormGroup, Input, Label, Button} from 'reactstrap'
+import { useParams } from 'react-router-dom'
+import {toast} from 'react-toastify';
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 const EditAdminMembership =  () =>  {
-    
+    const token = localStorage.getItem("token");
+    const history = useHistory()
     const [content,setContent] = useState('content') 
     const onChange = (evt) => {
         const newContent = evt.editor.getData();
         setContent(newContent)
     }
+
+    const params = useParams();
+    const [packageData, setPackageData] = useState('');
+    const [price, setPackagePrice] = useState({});
+   useEffect(() => {
+    const GetData = async () => {
+        const config = {
+    headers: {'Authorization': 'JWT '+token }
+  };
+      const result = await axios('/api/membership/'+`${params.id}`,config);
+      setPackageData(result.data.name);
+      setPackagePrice(result.data.price);
+      setContent(result.data.description);
+    };
+    GetData();
+  }, []);
+
+  const ChangeName = (e) => {
+    setPackageData(e.target.value);
+  }
+  
+  const ChangePrice = (e) => {
+    setPackagePrice(e.target.value);
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const config = {
+      headers: { 'Content-Type': 'application/json'  ,'Access-Control-Allow-Origin': '*' , 'Authorization': 'JWT '+token }
+      };
+      const bodyParameters = {
+        name: packageData,
+        description: content,
+        price: price
+
+      };
+      axios.put(`/api/membership/savepackage/`+`${params.id}`,
+        bodyParameters,
+        config
+      ) .then(response => {
+        toast.success("Page Content Updated!")
+        setTimeout(() => {
+          history.push('/dashboard/admin/admin-membership-package/');
+      }, 1000);
+      })
+         .catch(error => console.log('Form submit error', error))
+  };
 
     return (
             <Fragment>
@@ -22,12 +75,14 @@ const EditAdminMembership =  () =>  {
                             <Form className="form theme-form">
                               <FormGroup>
                                 <Label htmlFor="exampleFormControlInput">{"Membership Name"}</Label>
-                                <Input className="form-control"  type="name" />
+                                <Input className="form-control"  type="name" value = {packageData} onChange = {ChangeName}/>
                               </FormGroup>
                               <FormGroup>
                                 <Label htmlFor="exampleFormControlInput">{"Price"}</Label>
-                                <Input className="form-control"  type="number" />
+                                <Input className="form-control"  type="number" value = {price} onChange = {ChangePrice}/>
+                                
                               </FormGroup>
+                              
                               <CKEditors
                                   activeclassName="p10"
                                   content={content}
@@ -36,7 +91,7 @@ const EditAdminMembership =  () =>  {
                                   }}
                               />
                               <div className="m-t-20">
-                                <Button color="primary">{"Save"}</Button>
+                                <Button color="primary" onClick = {handleSubmit}>{"Save"}</Button>
                               </div>
                             </Form>
                           </CardBody>
