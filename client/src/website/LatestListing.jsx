ce34c94319712db5628096bc6f3beca3628e98eb
krishna mishra <er.krishna.mishra@gmail.com>
2022-05-05 15:27:04 +0530

    latest listing page

--- client/src/website/LatestListing.jsx
+++ client/src/website/LatestListing.jsx
@@ -7,7 +7,6 @@ import { Container, Row, Col, Card, CardTitle, CardText, Button } from 'reactstr
 import { useParams } from "react-router-dom";
 import axios from 'axios';
 import { BallTriangle } from  'react-loader-spinner'
-import Pagination from "react-js-pagination";
 import "react-multi-carousel/lib/styles.css";
 import InfiniteScroll from 'react-infinite-scroll-component';
 
@@ -17,18 +16,9 @@ const LatestListing = (props) => {
 
   const [items , setItems] = useState([]);
   const [hasMore, sethasMore] = useState(true);
-
-  const [vendorData, setVendorData] = useState([]);
-  const [activePage, setActivePage] = useState(0);
-  const [totalItemsCount, setTotalItemsCount] = useState(0); 
-  const [pageRangeDisplayed, setPageRangeDisplayed] = useState(20);  
-  const [pagesCount, setPagesCount] = useState(0);
   const [page, setPage] = useState(1);
   const [size, setSize] = useState(3);
-  const [filter, setFilter] = useState('');
-  const [categoryName, setCategoryName] = useState('');
   const [country, setCountry] = useState(localStorage.getItem('country_code'));
-  const [showPagination, setShowPagination] = useState(false);
   const [loader, setLoader] = useState(<BallTriangle color="#00BFFF" height={100} width={300} />);
   const [showMore, setShowMore] = useState(false);
 
@@ -50,23 +40,17 @@ const LatestListing = (props) => {
 
       await axios(config1)
       .then(function (result) {
-        console.log("START WITH FINISH END");
         setLoader('There are no business listing available');
         setItems(result.data.vendors);
-        setVendorData(result.data.vendors);
-        setTotalItemsCount(result.data.totalItems);  
-        setActivePage(result.data.currentPage);
-        setPagesCount(result.data.totalPages);
-        setShowPagination(((result.data.totalItems > 0 ) ?? true));
       })
       .catch(function (error) {
       });
 
-  
+
     };
     setPage(page + 1);
     getComments();
-   }, []);
+  }, []);
 
    const fetchComments = async () => {
 
@@ -88,275 +72,52 @@ const LatestListing = (props) => {
     setPage(page + 1);
  }
 
-
-  useEffect(() => {
-
-
-      // var config = {
-      //   method: 'get',
-      //   url: '/api/latest-additions/latest/all/',
-      //   headers: { 
-      //     'Content-Type': 'application/json'
-      //   },
-      //   params : {
-      //     'filter': filter,
-      //     'country': country,
-      //     'page': page,
-      //     'size': size,
-      //     'new' : 2
-      //   }
-      // };
-
-      // axios(config)
-      // .then(function (result) {
-      //   setLoader('There is no business listing available');
-      //   setVendorData(result.data.vendors);
-      //   setTotalItemsCount(result.data.totalItems);  
-      //   setActivePage(result.data.currentPage);
-      //   setPagesCount(result.data.totalPages);
-      //   setShowPagination(((result.data.totalItems > 0 ) ?? true));
-      // })
-      // .catch(function (error) {
-      // });
-
-
-      var config = {
-        method: 'get',
-        url: '/api/categories/getCatById/',
-        headers: { 
-          'Content-Type': 'application/json'
-        }
-      };
-
-      axios(config)
-      .then(function (result) {
-        setCategoryName(result.data.name);
-      })
-      .catch(function (error) {
-      });
-
-  }, []);
-
   const addDefaultSrc = (ev) => {
     ev.target.src = `${process.env.PUBLIC_URL}/assets/images/resturent/resturentimg1.jpg`;
-  }
-
-  const handlePreviousClick = (e) => {
-    e.preventDefault();
-    var config = {
-      method: 'get',
-      url: '/api/latest-additions/latest/all/',
-      headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      params : {
-        'page': activePage-1,
-        'size': size
-      }
-    };
-
-    axios(config)
-    .then(function (result) {
-      setVendorData(result.data.vendors);
-      setTotalItemsCount(result.data.totalItems);  
-      setActivePage(result.data.currentPage);
-      setPagesCount(result.data.totalPages);
-      setShowPagination(((result.data.totalItems > 0 ) ?? true));
-    })
-    .catch(function (error) {
-    });
-
-  };
-
-
-  const handleNextClick = (e) => {
-    e.preventDefault();
-    var config = {
-      method: 'get',
-      url: '/api/latest-additions/latest/all/',
-      headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      params : {
-        'filter': filter,
-        'country': country,
-        'page': activePage+1,
-        'size': size
-      }
-    };
-
-    axios(config)
-    .then(function (result) {
-      setVendorData(result.data.vendors);
-      setTotalItemsCount(result.data.totalItems);  
-      setActivePage(result.data.currentPage);
-      setPagesCount(result.data.totalPages);
-      setShowPagination(((result.data.totalItems > 0 ) ?? true));
-    })
-    .catch(function (error) {
-    });
-
-  };
-
-  const handlePageClick = (e, pageNumber) => {
-    e.preventDefault();
-    var config = {
-      method: 'get',
-      url: '/api/latest-additions/latest/all/',
-      headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      params : {
-        'filter': filter,
-        'country': country,
-        'page': pageNumber+1,
-        'size': size
-      }
-    };
-
-    axios(config)
-    .then(function (result) {
-      setVendorData(result.data.vendors);
-      setTotalItemsCount(result.data.totalItems);  
-      setActivePage(result.data.currentPage);
-      setPagesCount(result.data.totalPages);
-      setShowPagination(((result.data.totalItems > 0 ) ?? true));
-    })
-    .catch(function (error) {
-
-    });
-
-  };
-
-  const handleFirstClick = (e) => {
-    e.preventDefault();
-    var config = {
-      method: 'get',
-      url: '/api/latest-additions/latest/all/',
-      headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      params : {
-        'filter'  : filter,
-        'country' : country,
-        'page'    : page,
-        'size'    : size
-      }
-    };
-
-    axios(config)
-    .then(function (result) {
-      setVendorData(result.data.vendors);
-      setTotalItemsCount(result.data.totalItems);  
-      setActivePage(result.data.currentPage);
-      setPagesCount(result.data.totalPages);
-      setShowPagination(((result.data.totalItems > 0 ) ?? true));
-    })
-    .catch(function (error) {
-    }); 
-
-  };
-
-  const handleLastClick = (e) => {
-
-    e.preventDefault();
-    var config = {
-      method: 'get',
-      url: '/api/latest-additions/latest/all/'+'?filter='+filter+'&country='+country+'&page='+pagesCount+'&size='+size,
-      headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      params : {
-        'filter': filter,
-        'country': country,
-        'page': pagesCount,
-        'size': size
-      }
-    };
-
-    axios(config)
-    .then(function (result) {
-      setVendorData(result.data.vendors);
-      setTotalItemsCount(result.data.totalItems);  
-      setActivePage(result.data.currentPage);
-      setPagesCount(result.data.totalPages);
-      setShowPagination(((result.data.totalItems > 0 ) ?? true));
-    })
-    .catch(function (error) {
-    }); 
-
-  };
-
-
-  const handlePageChange = (pageNumber) => {
-
-    var config = {
-      method: 'get',
-      url: '/api/latest-additions/latest/all/',
-      headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      params : {
-        'filter': filter,
-        'country': country,
-        'page': pageNumber,
-        'size': size
-      }
-    };
-
-    axios(config)
-    .then(result=>{
-      setVendorData(result.data.vendors);
-      setTotalItemsCount(result.data.totalItems);  
-      setActivePage(result.data.currentPage);
-      setPagesCount(result.data.totalPages);
-      setShowPagination(((result.data.totalItems > 0 ) ?? true));
-    });
   }
 
   return (
       <Container fluid={true} className="p-0">
         <Header2/>
-        <SearchBar/>
         <Container>
           <div className="hotdeals LatestListing">
             <h4>{'LATEST ADDITIONS'}</h4>
             { (items && items.length) ? 
             <Row>
             <InfiniteScroll
-                    dataLength={items.length} 
-                    next={fetchData}
-                    hasMore={hasMore}
-                    loader={<h4></h4>}
-                    endMessage={
-                      <p style={{ textAlign: 'center' }}>
-                        <b>Yay! You have seen it all</b>
-                      </p>
-                    }>
-                      <Row>
-                              { (items).map((item , i) => (
-                                <Col sm="4" key={i}>
-                                    <div className="customcard">
-                                    <Card>
-                                      <div className="hImage">
-                                        <a href={`${process.env.PUBLIC_URL}/BusinessDetails/${item.user_id}`}>
-                                          <img onError={addDefaultSrc} src={`${process.env.PUBLIC_URL}/api/uploads/banner/${item.banner}`} />
-                                        </a>
-                                      </div>
-                                      <CardTitle tag="h5">
-                                        <a href={`${process.env.PUBLIC_URL}/BusinessDetails/${item.user_id}`}>{item.business_name}</a>
-                                      </CardTitle>
-                                      <CardText>
-                                        {(item.about_business && item.about_business.length >= 400) ?(item.about_business.substring(0, 400) + "...") : item.about_business}
-                                      </CardText>
-                                      <Button><a href={`${process.env.PUBLIC_URL}/BusinessDetails/${item.user_id}`}> SEE DETAILS</a></Button>
-                                    </Card>
-                                  </div>
-                                  </Col>
-                                  )) 
-                              }
-                      </Row>
-                </InfiniteScroll>
+              dataLength={items.length} 
+              next={fetchData}
+              hasMore={hasMore}
+              loader={loader}
+              endMessage={
+                <p style={{ textAlign: 'center' }}>
+                  <b>Yay! You have seen it all</b>
+                </p>
+              }>
+                <Row>
+                  { (items).map((item , i) => (
+                    <Col sm="4" key={i}>
+                        <div className="customcard">
+                        <Card>
+                          <div className="hImage">
+                            <a href={`${process.env.PUBLIC_URL}/BusinessDetails/${item.user_id}`}>
+                              <img onError={addDefaultSrc} src={`${process.env.PUBLIC_URL}/api/uploads/banner/${item.banner}`} />
+                            </a>
+                          </div>
+                          <CardTitle tag="h5">
+                            <a href={`${process.env.PUBLIC_URL}/BusinessDetails/${item.user_id}`}>{item.business_name}</a>
+                          </CardTitle>
+                          <CardText>
+                            {(item.about_business && item.about_business.length >= 400) ? (item.about_business.substring(0, 400) + "...") : item.about_business}
+                          </CardText>
+                          <Button><a href={`${process.env.PUBLIC_URL}/BusinessDetails/${item.user_id}`}> SEE DETAILS</a></Button>
+                        </Card>
+                      </div>
+                      </Col>
+                      )) 
+                  }
+                </Row>
+              </InfiniteScroll>
             </Row>  
              : <p style={{ textAlign:'center' }}>There is no listing found!</p> }
           </div>
