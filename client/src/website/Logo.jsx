f48d42af8f54047a39d87f78de28e2ad8b5823a6
ganesh <nganesh@n2rtechnologies.in>
2022-05-17 17:37:38 +0530

    upload logo

--- client/src/website/Logo.jsx
+++ client/src/website/Logo.jsx
@@ -1,12 +1,36 @@
-import React,{useState,useMemo,Fragment} from 'react';
+import React,{useState,useEffect} from 'react';
 import './css/header.css'
                       
 const Logo = () => {
+  const [logo, setLogo] = useState('');
+  useEffect(() => {
+  
+    const config = {
+        headers: { 'Content-Type': 'application/json'  ,'Access-Control-Allow-Origin': '*' , 'Authorization': 'JWT '+localStorage.getItem('token') }
+    };
+ 
+   fetch('/api/site-settings/getsettings' , config)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setLogo('/api/uploads/site/'+result.logo);
+        },
+        (error) => {
+          
+        }
+      )
+
+  }, []);
+
+  const defaultlogo = (e) => {
+    e.target.src = `${process.env.PUBLIC_URL}/assets/images/united-logo.png`;
+  }
+
 
   return (
       <div className="Logo">
         <a href="/">
-          <img src={`${process.env.PUBLIC_URL}/assets/images/united-logo.png`} alt="United Restaurants"/>
+          <img src={logo} onError = {defaultlogo} alt="United Restaurants"/>
         </a>
       </div>
   );
